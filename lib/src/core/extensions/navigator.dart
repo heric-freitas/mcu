2572ea2bd20cce45fb743d54d5ef5ebbbf90336c
Heric <hericfelipe4@gmail.com>
2023-05-17 09:59:50 -0300

    add pop

--- lib/src/core/extensions/navigator.dart
+++ lib/src/core/extensions/navigator.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 extension NavigatorExtenion on BuildContext {
   NavigatorState get _navigator => Navigator.of(this);
 
+  void pop<T extends Object?>([T? result]) => _navigator.pop(result);
+
   Future<T?> pushNamed<T extends Object?>(String routeName,
           {Object? arguments}) =>
       _navigator.pushNamed<T>(routeName, arguments: arguments);
