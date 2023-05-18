2c9bb533447c1793ceb144f43fb4496eee644058
Heric <hericfelipe4@gmail.com>
2023-05-18 00:23:41 -0300

    date formater

--- lib/src/core/extensions/date_formatter.dart
+++ lib/src/core/extensions/date_formatter.dart
@@ -1,9 +1,9 @@
 extension DateFormatter on String {
   String get formatterDatePTBR {
     final dateSplit = split('-');
-    final day = dateSplit[0];
+    final day = dateSplit[2];
     final month = dateSplit[1];
-    final year = dateSplit[2];
+    final year = dateSplit[0];
     return '$day/$month/$year';
   }
 }
